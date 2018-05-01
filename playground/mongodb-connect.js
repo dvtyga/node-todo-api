956f86f85cc8540c2d6082de4475cf8757c7e7ab
Tien D <devtyger@gmail.com>
2018-05-01 01:34:55 -0400

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// deconstructing
+// var user = {name: 'andrew', age: 25};
+// var {name} = user;
+// console.log(name);
+
+const url = 'mongodb://localhost:27017/TodoApp';
+MongoClient.connect(url , (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // db.collection('Users').insertOne({
+  //   name: 'Peter',
+  //   age: 24,
+  //   location: 'New York'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
+
+  db.close();
+});
